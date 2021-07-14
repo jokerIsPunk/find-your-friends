2c4bc3343d985261acbe6465eb22bb70b9a5c4ee
jokerIsPunk <79020414+jokerIsPunk@users.noreply.github.com>
2021-07-14 15:36:50 -0400

    Created usharp script version

---
+++ Find Your Friends/Scripts/FindYourFriends_us.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FindYourFriends_us : UdonSharpBehaviour
+{
+    public Text tipText;
+
+    private int scrollDir = 0;
+
+    void Start()
+    {
+        
+    }
+
+    public void _Left()
+    {
+        scrollDir = -1;
+        _Scroll();
+    }
+
+    public void _Right()
+    {
+        scrollDir = 1;
+        _Scroll();
+    }
+
+    public void _Scroll()
+    {
+
+    }
+}
