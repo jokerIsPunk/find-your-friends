0dfda9cad7c62e5c7876ceb2ffbc66a6f9bb3dc0
jokerIsPunk <79020414+jokerIsPunk@users.noreply.github.com>
2021-07-15 10:39:30 -0400

    more work on usharp version

--- Find Your Friends/Scripts/FindYourFriends_us.cs
+++ Find Your Friends/Scripts/FindYourFriends_us.cs
@@ -5,11 +5,20 @@ using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class FindYourFriends_us : UdonSharpBehaviour
 {
     public Text tipText;
 
     private int scrollDir = 0;
+    private int scrollPos = 696969;
+
+    private VRCPlayerApi[] allPlayers = new VRCPlayerApi[80];
+    private VRCPlayerApi[] visiblePlayers = new VRCPlayerApi[80];
+    [UdonSynced]
+    private int[] hiddenPlayers = new int[80];
+    [UdonSynced]
+    private int hiddenLength = 0;
 
     void Start()
     {
@@ -30,6 +39,32 @@ public class FindYourFriends_us : UdonSharpBehaviour
 
     public void _Scroll()
     {
+        scrollPos = scrollPos + scrollDir;
+        int visiblePlayersSize = 0;
+        int playerCount = VRCPlayerApi.GetPlayerCount();
+        for (int iAllPlayers = 0; iAllPlayers < playerCount; iAllPlayers++)
+        {
+            VRCPlayerApi player = allPlayers[iAllPlayers];
+            if (Utilities.IsValid(player))
+            {
+                int playerId = player.playerId;
+                bool foundHidden = false;
+                int hiddenId = 0;
+                for (int iHidden = 0; iHidden < hiddenLength; iHidden++)
+                {
+                    hiddenId = hiddenPlayers[iHidden];
+                    if (hiddenId == playerId)
+                    {
+                        foundHidden = true;
+                        break;
+                    }
+                }
 
+                // add final part of this block
+            }
+
+        }
+
+        // later parts of this block
     }
 }
