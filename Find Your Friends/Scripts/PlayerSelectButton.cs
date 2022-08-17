fd961286830df981f5d1ddc9cd39e66131bc4a3c
jokerIsPunk <79020414+jokerIsPunk@users.noreply.github.com>
2022-08-17 12:02:33 -0400

    changed color code for arrow

--- Find Your Friends/Scripts/PlayerSelectButton.cs
+++ Find Your Friends/Scripts/PlayerSelectButton.cs
@@ -13,6 +13,10 @@ namespace jokerispunk
     {
         public Text label;
         public Image arrow;
+
+        [Header("UI arrows color range")]
+        public Color color1 = Color.cyan;
+        public Color color2 = Color.red;
         [HideInInspector] public VRCPlayerApi vrcPlayer;
 
         public void _ConfigureButton(VRCPlayerApi playerInput)
@@ -20,9 +24,13 @@ namespace jokerispunk
             vrcPlayer = playerInput;
             label.text = playerInput.displayName;
 
-            // random color arrow between cyan and red
-            float hue = Random.Range(.5f, 1f);
-            Color col = Color.HSVToRGB(hue, 0.8f, 1f);
+            // random hue; uses color2's saturation and value
+            float hue1, hue2;
+            float s, v;
+            Color.RGBToHSV(color1, out hue1, out s, out v);
+            Color.RGBToHSV(color2, out hue2, out s, out v);
+            float hueRandom = Random.Range(hue1, hue2);
+            Color col = Color.HSVToRGB(hueRandom, s, v);
             arrow.color = col;
         }
 
